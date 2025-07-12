15d1d0c62b437bb2a23785bb1c262f76c3c0235f
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-07-13 00:39:56 +0300

    Some general files created

---
+++ utils/constants.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace constants {
+    constexpr int MAP_WIDTH = 102;
+    constexpr int MAP_HEIGHT = 52;
+    constexpr char MAP_BORDER_CHAR = '#';
+
+    constexpr int SNAKE_START_POSITION_X = MAP_WIDTH / 2;
+    constexpr int SNAKE_START_POSITION_Y = MAP_HEIGHT / 2;
+    constexpr char SNAKE_HEAD_UP_CHAR = '^';
+    constexpr char SNAKE_HEAD_RIGHT_CHAR = '>';
+    constexpr char SNAKE_HEAD_DOWN_CHAR = 'v';
+    constexpr char SNAKE_HEAD_LEFT_CHAR = '<';
+    constexpr char SNAKE_BODY_CHAR = '0';
+
+    constexpr char FOOD_CHAR = '*';
+}
