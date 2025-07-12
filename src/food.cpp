12d0181662081ebc993039be69667bd09316b9ae
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-07-13 02:57:23 +0300

    Food class implemented

---
+++ src/food.cpp
@@ -0,0 +1,17 @@
+#include "food.h"
+
+Food::Food() {
+}
+
+Food::~Food() {
+}
+
+void Food::GenerateNewPosition(std::vector<std::pair<int, int>> bodyPartPositions) {
+    do {
+        this->position.first = rand() % MAP_WIDTH - 1;
+        this->position.second = rand() % MAP_HEIGHT - 1;
+
+        if (this->position.first == 0) this->position.first++;
+        if (this->position.second == 0) this->position.second++;
+    } while (std::find(bodyPartPositions.begin(), bodyPartPositions.end(), this->position) != bodyPartPositions.end());
+}
---
+++ src/food.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+
+#include "../utils/constants.h"
+
+using namespace constants;
+
+class Food {
+public:
+    std::pair<int, int> position;
+
+public:
+    Food();
+    ~Food();
+
+    void GenerateNewPosition(std::vector<std::pair<int, int>> snakeBodyPartPositions);
+};
