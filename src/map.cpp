5f2d33bcffa632658325cc7171d4d70be0f881e6
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-07-13 18:04:48 +0300

    Map class and console controls implemented

--- src/map.cpp
+++ src/map.cpp
@@ -8,22 +8,80 @@ Map::~Map() {
 }
 
 void Map::Initialize() {
-    for (size_t i = 0; i < MAP_HEIGHT; i++) {
-        for (size_t j = 0; j < MAP_WIDTH; j++) {
-            if (i == 0 || i == MAP_HEIGHT - 1 || j == 0 || j == MAP_WIDTH - 1) {
-                map[i][j] = MAP_BORDER_CHAR;
+    for (size_t y = 0; y < MAP_HEIGHT; y++) {
+        for (size_t x = 0; x < MAP_WIDTH; x++) {
+            if (y == 0 || y == MAP_HEIGHT - 1 || x == 0 || x == MAP_WIDTH - 1) {
+                currentMap[y][x] = MAP_BORDER_CHAR;
             } else {
-                map[i][j] = ' ';
+                currentMap[y][x] = ' ';
+            }
+
+            previousMap[y][x] = ' ';
+        }
+    }
+
+    SetupConsole();
+}
+
+void Map::SetupConsole() {
+    HWND consoleWindow = GetConsoleWindow();
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    
+    COORD bufferSize = {static_cast<SHORT>(MAP_WIDTH), static_cast<SHORT>(MAP_HEIGHT + 2)};
+    SetConsoleScreenBufferSize(hConsole, bufferSize);
+    
+    SMALL_RECT windowSize = {0, 0, static_cast<SHORT>(MAP_WIDTH), static_cast<SHORT>(MAP_HEIGHT + 2)};
+    SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+
+    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+    style &= ~WS_MAXIMIZEBOX & ~WS_THICKFRAME;
+    SetWindowLong(consoleWindow, GWL_STYLE, style);
+    
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(hConsole, &cursorInfo);
+    cursorInfo.bVisible = false;
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+
+    SetConsoleTitle("C++ Snake Game");
+}
+
+void Map::SetCursorPosition(size_t x, size_t y) {
+    COORD coord = {static_cast<SHORT>(x), static_cast<SHORT>(y)};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void Map::Update(const std::shared_ptr<Snake> snake, const std::shared_ptr<Food> food) {
+    for (size_t y = 0; y < MAP_HEIGHT; y++) {
+        for (size_t x = 0; x < MAP_WIDTH; x++) {
+            if (y == 0 || y == MAP_HEIGHT - 1 || x == 0 || x == MAP_WIDTH - 1) {
+                currentMap[y][x] = MAP_BORDER_CHAR;
+            } else {
+                currentMap[y][x] = ' ';
             }
         }
     }
+
+    for (size_t i = 0; i < snake->bodyPartPositions.size(); i++) {
+        if (i == snake->bodyPartPositions.size() - 1) {
+            currentMap[snake->bodyPartPositions[i].second][snake->bodyPartPositions[i].first] = snake->directionChars[snake->direction];
+        } else {
+            currentMap[snake->bodyPartPositions[i].second][snake->bodyPartPositions[i].first] = SNAKE_BODY_CHAR;
+        }
+    }
+
+    currentMap[food->position.second][food->position.first] = FOOD_CHAR;
 }
 
 void Map::Draw() {
-    for (size_t i = 0; i < MAP_HEIGHT; i++) {
-        for (size_t j = 0; j < MAP_WIDTH; j++) {
-            std::cout << map[i][j];
+    for (size_t y = 0; y < MAP_HEIGHT; y++) {
+        for (size_t x = 0; x < MAP_WIDTH; x++) {
+            if (previousMap[y][x] != currentMap[y][x]) {
+                SetCursorPosition(x, y);
+                std::cout << currentMap[y][x];
+                previousMap[y][x] = currentMap[y][x];
+            }
         }
-        std::cout << std::endl;
     }
+
+    std::cout.flush();
 }
--- src/map.h
+++ src/map.h
@@ -1,21 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
+#include <vector>
+#include <utility>
+#include <windows.h>
 
 #include "../utils/constants.h"
+#include "snake.h"
+#include "food.h"
 
 using namespace constants;
 
 class Map {
-public:
-    char map[MAP_HEIGHT][MAP_WIDTH];
+private:
+    char previousMap[MAP_HEIGHT][MAP_WIDTH];
+    char currentMap[MAP_HEIGHT][MAP_WIDTH];
 
 public:
     Map();
     ~Map();
 
+    void SetCursorPosition(size_t x, size_t y);
+    void Update(const std::shared_ptr<Snake> snake, const std::shared_ptr<Food> food);
     void Draw();
 
 private:
     void Initialize();
+    void SetupConsole();
 };
